7e690ef76da48f3d157b139fedc4c2a9770930bf
jussi <45388631+jussihailu1@users.noreply.github.com>
2020-04-07 21:15:15 +0200

    All the html and css is in place. Next up: js -> ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import {MenuComponent} from './main/menu/menu.component';
+import {MenuBottomComponent} from './main/menu-bottom/menu-bottom.component';
+import { FooterComponent } from './main/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainComponent,
+    MenuComponent,
+    MenuBottomComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
