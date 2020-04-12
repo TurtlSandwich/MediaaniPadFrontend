c1e34d1d60951a2ce12f53658a1cc7ff87b7e545
jussi <45388631+jussihailu1@users.noreply.github.com>
2020-04-12 15:20:43 +0200

    .

--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -5,5 +5,12 @@ export class Order{
     constructor(private dishes: Dish[]) {
         
     }
-    
+
+    addDish(dish: Dish){
+        this.dishes.push(dish);
+    }
+
+    removeDish(id: Number){
+        this.dishes = this.dishes.filter(dish => dish.getId() != id);
+    }
 }
